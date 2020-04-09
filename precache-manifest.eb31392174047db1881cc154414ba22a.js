66cccee7ec7eb934a6175242a9bfc4d752f3599f
Zver64 <coolvill@mail.ru>
2020-04-10 02:28:58 +0300

    Updates

---
+++ precache-manifest.eb31392174047db1881cc154414ba22a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860557ef2a76bd9cb096f48a81fe61b2",
+    "url": "/Rick-and-Morty/index.html"
+  },
+  {
+    "revision": "85e1e475c249636fed48",
+    "url": "/Rick-and-Morty/static/css/main.75956eaa.chunk.css"
+  },
+  {
+    "revision": "eb46db899ff0a841272d",
+    "url": "/Rick-and-Morty/static/js/2.c45aca55.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Rick-and-Morty/static/js/2.c45aca55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e1e475c249636fed48",
+    "url": "/Rick-and-Morty/static/js/main.82730bc5.chunk.js"
+  },
+  {
+    "revision": "192756b6eedada9798ce",
+    "url": "/Rick-and-Morty/static/js/runtime-main.b7a04899.js"
+  },
+  {
+    "revision": "f6338795f4bed1b917ef0546c3c2b740",
+    "url": "/Rick-and-Morty/static/media/vector.f6338795.svg"
+  }
+]);
