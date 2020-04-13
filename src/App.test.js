79c0d8acbd40ed867ec69f79ffab4b0f486776b2
Zver64 <coolvill@mail.ru>
2020-04-13 18:19:22 +0300

    added basic test that is not working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import App from './App'
+import { GET_PARTY } from './apollo/gql'
+import resolvers from './apollo/resolvers'
+
+const mocks = [
+  {
+    request: {
+      query: GET_PARTY,
+    },
+    result: {
+      data: {
+        __typename: 'Root',
+        party: {
+          __typename: 'Party',
+          rick: 'test rick',
+          morty: 'test morty',
+        },
+      },
+    },
+  },
+]
+
+afterEach(cleanup)
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const { getByTestId } = render(
+    <MockedProvider mocks={mocks} resolvers={resolvers} addTypename={false}>
+      <App />
+    </MockedProvider>
+  )
+  // expect(getByTestId('test')).toHaveClass('main3')
+})
